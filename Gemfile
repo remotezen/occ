b99404b656ed5ed86baab07bdcb6ea3481381c5e
Fred Hudson <remotezen@bell.net>
2013-03-26 14:19:06 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.0.beta1'
+
+
+group :assets do
+  gem 'sass-rails',   '4.0.0.beta1'
+  gem 'coffee-rails', '4.0.0.beta1'
+  gem 'uglifier', '1.0.3'
+end
+
+gem 'jquery-rails', '2.2.1'
+gem 'turbolinks', '1.0.0'
+gem 'jbuilder', '1.0.1'
+gem 'pg', '0.14.1'
+gem 'execjs'
+gem 'therubyracer'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Occ::Application.config.secret_key_base = '5bccf4d7738c2088b192163c774462a70d1b8434162d4a426d939641650cb1c4b509dcd4512c557f4e94d7cb5ce4e41fada7584a7abc64e5d8a9b12061b6e9ff'
